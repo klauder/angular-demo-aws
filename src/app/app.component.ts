eab8d4b72bcfcf8dcce88d4aaa160988198c6e88
Klauder <klauderdias@gmail.com>
2024-08-29 15:18:49 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { DatePipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  providers:[DatePipe]
+
+})
+
+export class AppComponent {
+  title = 'frontend';
+}
